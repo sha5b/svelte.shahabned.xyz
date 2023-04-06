89a23300a73cd7e8c8b2dd814b7e4b49d7b10009
sha5b <ned.tabulov@gmail.com>
2023-04-06 10:07:35 +0200

    fix

--- src/lib/components/elements/Itemcard.svelte
+++ src/lib/components/elements/Itemcard.svelte
@@ -95,12 +95,8 @@
 		<div>
 			<image-overlay />
 			{#if type === 'video'}
-				<video
-					autoplay
-					muted
-					loop
-					src={getImageURL(collectionId, id, thumb, '0x250')}
-					alt={`${title}`}><track kind="captions" /></video>
+				<video autoplay muted loop src={getImageURL(collectionId, id, thumb)} alt={`${title}`}
+					><track kind="captions" /></video>
 			{:else if type === 'image'}
 				<img src={getImageURL(collectionId, id, thumb, '0x250')} alt={`${title}`} />
 			{:else if type === '3d'}
